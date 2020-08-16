87545313e4253fad8bb2085ac30fa52810e1c8ad
godats7 <49473818+godats7@users.noreply.github.com>
2020-08-16 19:28:23 +0900

    Add files via upload

---
+++ practice50.py
@@ -0,0 +1,60 @@
+# pass
+
+class Unit: #일반 유닛  부모클래스
+    def __init__(self,name,hp,speed): #__init__은 생성자
+        self.name = name 
+        self.hp = hp 
+        self.speed =speed     
+    def move(self,  location):
+        print("지상유닛 이동")
+        print("{0} : {1} 방향으로 이동합니다. [속도 {2}]".format(self.name,location,self.speed))
+
+class AttackUnit(Unit): # Unit을 상속하는 AttackUnit  자식클래스
+    def __init__(self,name,hp,speed,damage): 
+        Unit.__init__(self, name,hp,speed) #Unit에서 이름과 체력을 가져놈        
+        self.damage = damage
+    def attack(self, location)    :
+        print("{0} : {1}방향으로 적군을 공격합니다. [공격력은 {2}]".\
+            format(self.name,location,self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1}데미지를 입었습니다.".format(self.name,damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1}입니다.".format(self.name,self.hp))
+        if self.hp <=0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
+# 드랍쉽 : 공중 유닛, 수송기, 다른 지상유닛을 수송, 공격x
+class Flyable : 
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+    
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
+            .format(name,location,self.flying_speed))
+
+# 공중 공격 유닛 클래 스
+class FlyableAttackUnit(AttackUnit,Flyable):
+    def __init__(self,name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp,0,damage) #지상스피드는 0
+        Flyable.__init__(self,flying_speed)
+    def move(self, location):
+        print("공중유닛 이동")
+        self.fly(self.name, location)
+
+# 건물
+class BuildingUnit(Unit):
+    def __init__(self,name,hp,location):
+        pass
+
+# 서플라이 디폿 : 건물, 1개 건물=8 유닛.
+supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+
+def game_over():
+    pass
+
+game_start()
+game_over()
---
+++ practice_class.py
@@ -0,0 +1,22 @@
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+class FlyableUnit(Unit,Flyable):
+    def __init__(self):
+        # super().__init__()
+        Unit.__init__(self)
+        Flyable.__init__(self)
+
+class FlyableUnit2(Flyable,Unit):
+    def __init__(self):
+        super().__init__()
+
+#드랍쉽만들기
+
+dropship = FlyableUnit()
+dropship2 =FlyableUnit2()
